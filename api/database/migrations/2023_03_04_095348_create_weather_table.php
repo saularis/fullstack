a2ef653a7ce8dd528124792df93c1d50e2cc1eac
Abdulla Malik <hi@abdulla.dev>
2023-03-05 02:44:20 +0600

    make sure on delete cascade

--- api/database/migrations/2023_03_04_095348_create_weather_table.php
+++ api/database/migrations/2023_03_04_095348_create_weather_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('weather', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained()->onUpdate('cascade')->onDelete('cascade');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->float('temperature')->default(0);
             $table->float('feels_like')->default(0);
             $table->float('temp_min')->default(0);
